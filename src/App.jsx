16441df7d72c72094dff3c6b55c605bfcd9f6558
Eduardo Garcia <edugarciasb16@gmail.com>
2025-02-07 19:32:25 +0100

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+import Header from './components/Header';
 const App = () => {
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline text-red">
-                Hello world!
-            </h1>
-        </div>
+        <>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Header />
+            </div>
+            <ButtonGradient />
+        </>
     );
 };
 
