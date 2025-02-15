eb5bc4e9a60024dd5463c7fcfefa4a1fb3116a5b
Eduardo Garcia <edugarciasb16@gmail.com>
2025-02-15 21:23:18 +0100

    fix: remove cursor-pointer class

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -22,12 +22,12 @@ const Benefits = () => {
                                 backgroundImage: `url(${item.backgroundUrl})`,
                             }}
                         >
-                            <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem]">
+                            <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                                 <h5 className="h5 mb-5">{item.title}</h5>
                                 <p className="body-2 mb-6 text-n-3">
                                     {item.text}
                                 </p>
-                                <div className="flex items-center mt-auto cursor-pointer">
+                                <div className="flex items-center mt-auto">
                                     <img
                                         src={item.iconUrl}
                                         alt={item.title}
