9b6d2450974194d04d3180825d971bc64660b599
Eduardo Garcia <edugarciasb16@gmail.com>
2025-02-16 23:25:17 +0100

    Remove tagline component

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -4,7 +4,6 @@ import { roadmap } from '../constants';
 import { Gradient } from './design/Roadmap';
 import { check2, grid, loading1 } from '../assets';
 import Button from './Button';
-import Tagline from './Tagline';
 
 const Roadmap = () => {
     return (
